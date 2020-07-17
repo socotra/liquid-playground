93b436ac08cb537448765c6e398c73f2119b2f01
Will Barley <will.barley@socotra.com>
2020-07-17 10:50:49 -0400

    build: add s3-deploy plugin
    To deploy built project to S3 for hosting

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: "", // relative paths: So that the built project can be served from a path other than root
+
+  // relative paths: So that the built project can be served from a path other than root
+  publicPath: "",
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: "default",
+      overrideEndpoint: false,
+      region: "us-east-1",
+      bucket: "socotra.wb",
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: "index.html",
+      staticErrorPage: "index.html",
+      assetPath: "dist",
+      assetMatch: "**",
+      deployPath: "liquid-playground",
+      acl: "public-read",
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: "4.0.0-rc3",
+      uploadConcurrency: 5,
+    },
+  },
 };
