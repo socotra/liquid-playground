5b42bcf550c74605affd82b993cdea290c1a766c
Will Barley <will.barley@socotra.com>
2020-07-17 16:57:09 -0400

    feat: socotra logo, links to Github repo and report-an-issue, show version number

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,52 @@
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Socotra Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src="require('./assets/socotra-logo-with-name.png')"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          width="150"
         />
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer />
+      Liquid Playground
+      <v-spacer />
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            :href="
+              'https://github.com/socotra/liquid-playground/releases/tag/' +
+              version
+            "
+            target="_blank"
+            text
+            v-bind="attrs"
+            v-on="on"
+          >
+            <span class="mr-2">{{ version }}</span>
+            <v-icon>mdi-github</v-icon>
+          </v-btn>
+        </template>
+        <span>Visit this release on Github</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            href="https://github.com/socotra/liquid-playground/issues"
+            target="_blank"
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-alert</v-icon>
+          </v-btn>
+        </template>
+        <span>Tell me about a problem</span>
+      </v-tooltip>
     </v-app-bar>
 
     <v-main>
@@ -50,7 +68,7 @@ export default {
   },
 
   data: () => ({
-    //
+    version: "v0.1.0",
   }),
 };
 </script>
