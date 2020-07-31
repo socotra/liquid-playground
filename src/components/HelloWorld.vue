68118978814957c48580f7f668e9d5ebe6c05bba
Will Barley <will.barley@socotra.com>
2020-07-31 12:01:01 -0400

    fix: make default perilID a working peril

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -105,7 +105,7 @@ export default {
     password: "socotra",
     token: "",
     authorizationErrorMessage: "",
-    perilId: "100000011",
+    perilId: "100000010",
     perilErrorMessage: "",
     liquid:
       "{% assign base_rate = 21 %}\n" +
